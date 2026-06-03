c19ff2b7fda6fac9c0e99ebcd54925c7bfe00d48
brindha68686868-lang <brindha68686868@gmail.com>
2026-06-03 12:57:45 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,27 @@
+# List Example
+fruits = ["Apple", "Banana", "Mango"]
+
+print("List:")
+print(fruits)
+
+# Adding an item to the list
+fruits.append("Orange")
+print("After adding Orange:", fruits)
+
+# Changing an item in the list
+fruits[1] = "Grapes"
+print("After changing Banana to Grapes:", fruits)
+
+print("\n-------------------\n")
+
+# Tuple Example
+colors = ("Red", "Green", "Blue")
+
+print("Tuple:")
+print(colors)
+
+# Accessing tuple elements
+print("First color:", colors[0])
+
+# Tuples cannot be modified
+# colors[1] = "Yellow"  # This will cause an error
