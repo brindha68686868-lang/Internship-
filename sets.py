c6c17f7a7a40c0626182f2975abc5fc2e7762eaf
brindha68686868-lang <brindha68686868@gmail.com>
2026-06-03 20:05:54 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,23 @@
+# Dictionary Example
+student = {
+    "name": "Brindha",
+    "age": 19,
+    "course": "Python"
+}
+
+print("Student Details:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+
+# Set Example
+numbers = {10, 20, 30, 20, 40, 10}
+
+print("\nSet Elements:")
+print(numbers)
+
+# Adding an element to the set
+numbers.add(50)
+
+print("After Adding 50:")
+print(numbers)
